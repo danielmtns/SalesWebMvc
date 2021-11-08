8398fe4edb0679911cc0cefc67578544780ed3c8
sidekool <sidekoolz@gmail.com>
2021-11-07 22:03:39 -0300

    Simple Create Form

---
+++ SalesWebMvc/Migrations/20211024151847_OtherEntities.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SalesWebMvc.Migrations
+{
+    public partial class OtherEntities : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Sellers",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    BirthDate = table.Column<DateTime>(nullable: false),
+                    BaseSalary = table.Column<double>(nullable: false),
+                    DepartmentID = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sellers", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Sellers_Department_DepartmentID",
+                        column: x => x.DepartmentID,
+                        principalTable: "Department",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SalesRecord",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Date = table.Column<DateTime>(nullable: false),
+                    Amount = table.Column<double>(nullable: false),
+                    Status = table.Column<int>(nullable: false),
+                    SellerID = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SalesRecord", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_SalesRecord_Sellers_SellerID",
+                        column: x => x.SellerID,
+                        principalTable: "Sellers",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SalesRecord_SellerID",
+                table: "SalesRecord",
+                column: "SellerID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sellers_DepartmentID",
+                table: "Sellers",
+                column: "DepartmentID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SalesRecord");
+
+            migrationBuilder.DropTable(
+                name: "Sellers");
+        }
+    }
+}
---
+++ SalesWebMvc/Migrations/20211024230431_UpdateSeller.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SalesWebMvc.Migrations
+{
+    public partial class UpdateSeller : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SalesRecord_Sellers_SellerID",
+                table: "SalesRecord");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Sellers_Department_DepartmentID",
+                table: "Sellers");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Sellers",
+                table: "Sellers");
+
+            migrationBuilder.RenameTable(
+                name: "Sellers",
+                newName: "Seller");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Sellers_DepartmentID",
+                table: "Seller",
+                newName: "IX_Seller_DepartmentID");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Seller",
+                table: "Seller",
+                column: "ID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SalesRecord_Seller_SellerID",
+                table: "SalesRecord",
+                column: "SellerID",
+                principalTable: "Seller",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Seller_Department_DepartmentID",
+                table: "Seller",
+                column: "DepartmentID",
+                principalTable: "Department",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SalesRecord_Seller_SellerID",
+                table: "SalesRecord");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Seller_Department_DepartmentID",
+                table: "Seller");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Seller",
+                table: "Seller");
+
+            migrationBuilder.RenameTable(
+                name: "Seller",
+                newName: "Sellers");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Seller_DepartmentID",
+                table: "Sellers",
+                newName: "IX_Sellers_DepartmentID");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Sellers",
+                table: "Sellers",
+                column: "ID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SalesRecord_Sellers_SellerID",
+                table: "SalesRecord",
+                column: "SellerID",
+                principalTable: "Sellers",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Sellers_Department_DepartmentID",
+                table: "Sellers",
+                column: "DepartmentID",
+                principalTable: "Department",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
